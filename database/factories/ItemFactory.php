f1872b243a66be809b7729aed12d33c53871fdd5
nasubimen <93132252+nasubimen@users.noreply.github.com>
2023-02-23 18:08:36 +0900

    初期設定・Factory作成

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Item;
+
+class ItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+
+        $goods = ['A商品', 'B商品', 'C商品', 'D商品', 'E商品', 'F商品', 'G商品', 'H商品'];
+        return [
+            'name' => $this->faker->randomElement($goods),
+            'status' => "active",
+            'type' => $this->faker->numberBetween(0, 2),
+            'detail' => $this->faker->realText(50),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create()->each(function ($user) {
+            \App\Models\Item::factory(3)->create([
+                'user_id' => $user->id
+            ]);
+        });
     }
 }
