dae73f4fe57c6db0ca5f45df6290b51389cead9c
nasubimen <93132252+nasubimen@users.noreply.github.com>
2023-03-05 17:28:10 +0900

    タイトル名変更

--- resources/views/item/show.blade.php
+++ resources/views/item/show.blade.php
@@ -1,9 +1,9 @@
 @extends('adminlte::page')
 
-@section('title', '商品登録')
+@section('title', '商品詳細')
 
 @section('content_header')
-    <h1>商品登録</h1>
+    <h1>詳細</h1>
 @stop
 
 @section('content')
--- resources/views/log/index.blade.php
+++ resources/views/log/index.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', '商品一覧')
+@section('title', '履歴一覧')
 
 @section('content_header')
     <h1>履歴一覧</h1>
